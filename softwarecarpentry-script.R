23beeaaab4b1d8b6c05b73fdb2700d14fb464802
Meeta Mistry <meeta.mistry@gmail.com>
2015-03-16 18:30:52 -0400

    Delete softwarecarpentry-script.R

--- softwarecarpentry-script.R
+++
@@ -1,9 +0,0 @@
-## SOFTWARE CARPENTRY INTRO TO R 
-## This file contains the code for all activities carried out in this lecture
-
-
-# Read in metadata
-metadata <- read.csv('meta//mouse_exp_design.csv')
-
-# Read in data file
-data <- read.csv("data/counts.rpkm", header=T, sep=",", row.names=1)
